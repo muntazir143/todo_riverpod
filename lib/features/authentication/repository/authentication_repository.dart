8dce34bd930ab41318f2b3ba54574678b00e68b5
Muntazir Rizvi <smmr143@gmail.com>
2024-01-29 03:24:34 +0000

    auth using firebase auth and sqflite to store

--- lib/features/authentication/repository/authentication_repository.dart
+++ lib/features/authentication/repository/authentication_repository.dart
@@ -15,19 +15,32 @@ class AuthenticationRepository {
   Future<void> sendOTP(
       {required BuildContext context, required String phoneNumber}) async {
     await auth.verifyPhoneNumber(
-        phoneNumber: phoneNumber,
-        verificationCompleted: (credential) {
-          auth.signInWithCredential(credential);
-        },
-        verificationFailed: (exception) {
-          debugPrint(exception.message);
-          CoreUtils.showSnackBar(
-              context: context, message: '${exception.message}');
-        },
-        codeSent: (verificationId, _) {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (_) => const OTPVerificationScreen()));
-        },
-        codeAutoRetrievalTimeout: (verificationId) {});
+      phoneNumber: phoneNumber,
+      verificationCompleted: (credential) {
+        auth.signInWithCredential(credential);
+      },
+      verificationFailed: (exception) {
+        debugPrint(exception.message);
+        CoreUtils.showSnackBar(
+            context: context, message: '${exception.message}');
+      },
+      codeSent: (verificationId, _) {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (_) => const OTPVerificationScreen()));
+      },
+      codeAutoRetrievalTimeout: (verificationId) {},
+    );
+  }
+
+  Future<void> verifyOTP(
+      {required BuildContext context,
+      required String verificationId,
+      required String otp}) async {
+    final credential = PhoneAuthProvider.credential(
+        verificationId: verificationId, smsCode: otp);
+
+    final userCredential = await auth.signInWithCredential(credential);
+
+    if (userCredential.user != null) {}
   }
 }
